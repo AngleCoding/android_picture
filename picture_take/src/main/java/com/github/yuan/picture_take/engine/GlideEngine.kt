4354008d789881004f2e3082e4a5499730d7d19a
刘红鹏 <1016305858@qq.com>
2023-04-23 15:52:14 +0800

    修改图片加载

--- picture_take/src/main/java/com/github/yuan/picture_take/engine/GlideEngine.kt
+++ picture_take/src/main/java/com/github/yuan/picture_take/engine/GlideEngine.kt
@@ -2,6 +2,7 @@ package com.github.yuan.picture_take.engine
 
 import android.content.Context
 import android.widget.ImageView
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.github.yuan.picture_take.R
@@ -24,6 +25,11 @@ object GlideEngine : ImageEngine {
     }
 
     override fun loadImage(context: Context, url: String, imageView: ImageView) {
+        GlideApp.with(context)
+            .load(url)
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(imageView)
     }
 
     override fun loadImage(
@@ -36,6 +42,8 @@ object GlideEngine : ImageEngine {
 
         GlideApp.with(context)
             .load(url)
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(imageView)
     }
 
@@ -48,6 +56,8 @@ object GlideEngine : ImageEngine {
             .load(url)
             .override(180, 180)
             .sizeMultiplier(0.5f)
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .transform(CenterCrop(), RoundedCorners(8))
             .placeholder(R.drawable.ps_image_placeholder)
             .into(imageView)
@@ -62,6 +72,8 @@ object GlideEngine : ImageEngine {
             .load(url)
             .override(200, 200)
             .centerCrop()
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .placeholder(R.drawable.ps_image_placeholder)
             .into(imageView)
     }
